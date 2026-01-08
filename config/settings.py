4f462050bbdce1922bc444b3f315f148253b58c7
Oluwasomidotun <victoroluwasomidotun@gmail.com>
2026-01-08 14:40:47 +0100

    changes

--- config/settings.py
+++ config/settings.py
@@ -222,6 +222,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://hotcrowd-web-player.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
